29a0eba931a961694883e7ad6bd2748df233fff4
corentin.rozet@epitech.eu <corentin.rozet@epitech.eu@epitech.eu>
2020-04-25 14:21:52 +0200

    [ADD] matchstick --> usage.py

---
+++ matchstick/matchstick/usage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+def help():
+
+    """
+    Show usage of the program.
+    """
+
+    print("USAGE\n"
+        "\t./matchstick lines max\n\n"
+        "DESCRIPTION\n"
+        "\tlines\tthe number of lines (n in ]0, 100])\n"
+        "\tmax\tthe maximum number of matchsticks that can be taken out each turn (must be > 0)")
