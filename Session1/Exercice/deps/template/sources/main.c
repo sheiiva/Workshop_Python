a269025d45b3f021f2d6e89c1855c3f8729e2e98
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-12-03 15:30:55 +0100

    [ADD] Session1: repo init

---
+++ Session1/Exercice/deps/template/sources/main.c
@@ -0,0 +1,15 @@
+/*
+** W O R K S H O P  P Y T H O N
+** Corentin COUTRET-ROZET
+**
+** SOURCES//
+** main.c
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    return (0);
+}
---
+++ Session1/Exercice/sources/rinit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+######################################################
+#                                                    #
+#                Workshop Python3 (1/4)              #
+#                      ---------                     #
+#                Corentin COUTRET-ROZET              #
+#                                                    #
+# Github: https://github.com/sheiiva/Workshop_Python #
+######################################################
+
+PATH_TO_TEMPLATE = "deps/template"
